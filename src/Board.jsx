6aae7c7892c3bc18518179ccde063c1cf0e2f1b0
Clarence Coronel <clarence.coronel.r@gmail.com>
2024-02-11 16:50:07 +0800

    Proper useState implementation for inserting moves

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,55 +1,55 @@
 import { useState } from "react";
 
-function Tile({ index, eventHandler }){
+function Tile({eventHandler, content }) {
     return (
-        <div data-index={index} onClick={eventHandler} 
-        className="
-        bg-slate-100 
-        md:bg-neutral-200/70 
-        md:hover:bg-neutral-200/90 
-        rounded-lg w-[100%] 
-        h-[25vw] 
-        md:w-[10vw] 
-        md:h-[10vw]
-        md:hover:cursor-pointer
-        flex
-        justify-center
-        items-center
-        text-cyan-600
-        font-bold
-        text-3xl
-        font-mono">
-            
+        <div 
+            onClick={eventHandler} 
+            className="
+                bg-slate-100 
+                md:bg-neutral-200/70 
+                md:hover:bg-neutral-200/90 
+                rounded-lg 
+                w-[100%] 
+                h-[25vw] 
+                md:w-[10vw] 
+                md:h-[10vw]
+                md:hover:cursor-pointer
+                flex
+                justify-center
+                items-center
+                text-cyan-600
+                font-bold
+                text-3xl
+                font-mono"
+        >
+            {content}
         </div>
     );
 }
 
+function Board() {
+    const [currentMove, setCurrentMove] = useState("X");
+    const [board, setBoard] = useState(Array(9).fill(""));
 
-
-function Board(){
-    const [turn, setTurn] = useState("X");
-    const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
-
-    // const board = [ "", "" ,"",
-    //                 "", "" ,"",
-    //                 "", "" ,""]
-
-    const tiles = [];
-
-    function setMove(element){
-        console.log(element)
-        setTurn("O");
-    }
-
-    for(let i = 0; i < 9; i++){
-        tiles.push(<Tile index={i} key={i} eventHandler={setMove}/>);
-    }
+    const moveHandler = (index) => {
+        if (board[index] !== "") return;
+        const newBoard = [...board];
+        newBoard[index] = currentMove;
+        setBoard(newBoard);
+        setCurrentMove(currentMove === "X" ? "O" : "X");
+    };
 
     return (
         <div className="grid grid-rows-3 grid-cols-3 w-full h-fit gap-3 justify-items-center md:w-fit">
-           {tiles}
+            {board.map((content, index) => (
+                <Tile 
+                    key={index} 
+                    eventHandler={() => moveHandler(index)} 
+                    content={content} 
+                />
+            ))}
         </div>
     );
 }
 
-export default Board
+export default Board;
