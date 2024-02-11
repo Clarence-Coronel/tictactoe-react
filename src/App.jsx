0b1d94a300b566c366c34ba35f91b313d6e7de31
Clarence Coronel <clarence.coronel.r@gmail.com>
2024-02-11 14:01:53 +0800

    Initial commit
    Setup react and tailwind-css

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='w-screen h-screen bg-neutral-800 text-green-400 flex justify-center items-center'>Hello World</h1>
+    </>
+  )
+}
+
+export default App
